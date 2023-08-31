5c0006fb2f5391b94a445dadd31d7e68fc68c030
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2023-08-31 18:25:52 -0300

    :scroll: Added service logic

---
+++ src/services/ContractService.java
@@ -0,0 +1,17 @@
+package services;
+
+import model.Contract;
+
+public class ContractService {
+	
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	public void processContract(Contract contract, int months) {
+		
+	}
+	
+}
